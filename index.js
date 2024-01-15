f6ecbc025b594e4586746d5b324c564a79e83524
wolfiee42 <saifalislam2022@gmail.com>
2024-01-15 11:10:11 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 const express = require('express');
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-// const dbConnet = async () => {
-// try {
-    
-// } catch (error) {
-    
-// }
-// }
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@yumsynccluster.12ajabi.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+//testing whether mongodb is connected or not
+const dbConnet = async () => {
+    try {
+        client.connect();
+        console.log("DB Connected successfully!");
+    } catch (error) {
+        console.log(error.name, error.message);
+    }
+}
+dbConnet();
+
+
 
 
 app.get("/", (req, res) => {
